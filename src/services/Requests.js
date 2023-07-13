8155ddf841bdcac054c9e6e2656e2d5150296f9a
David Aremu <segun8427@gmail.com>
2023-07-13 21:50:06 +0100

    added address show

--- src/services/Requests.js
+++ src/services/Requests.js
@@ -7,100 +7,101 @@ const Requests = {};
 const Request = axios.create({
     baseURL: '',
     timeout: 15000,
-    headers: {'X-Powered-By': 'Bloom', 'Authorization': `Bearer ${localStorage.getItem('token')}`},
-  });
+    headers: { 'X-Powered-By': 'Bloom', 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+});
 
 
- Requests.Login = async (email, password) => {
+Requests.Login = async (email, password) => {
     try {
         let res = await Request.post(endpoints.login, {
-            'email'     : email,
-            'password'  : password
+            'email': email,
+            'password': password
         });
         if (res.data.status == 'error') {
             throw new Error(res.message)
         }
         return res.data;
-        
+
     } catch (error) {
         throw new Error(error);
     }
-  }
+}
 
- Requests.InvoiceList = async (url) => {
+Requests.InvoiceList = async (url) => {
     try {
         let res = await Request.get(url ? url : endpoints.invoiceList);
         return res.data;
-        
+
     } catch (error) {
         checkAuth(error);
         throw new Error(error);
     }
-  }
+}
 
- Requests.InvoiceView = async (id) => {
+Requests.InvoiceView = async (id) => {
     try {
-        let res = await Request.get( endpoints.invoiceView(id));
+        let res = await Request.get(endpoints.invoiceView(id));
         console.log(res.data.data[0]);
         return res.data.data[0];
     } catch (error) {
         checkAuth(error);
         throw new Error(error);
     }
-  }
+}
 
- Requests.InvoiceCreate = async (data) => {
+Requests.InvoiceCreate = async (data) => {
     try {
-        let res = await Request.post( endpoints.invoiceCreate,  data);
+        let res = await Request.post(endpoints.invoiceCreate, data);
         return res.data;
-        
+
     } catch (error) {
         checkAuth(error);
         throw new Error(error);
     }
-  }
+}
 
- Requests.InvoiceUpdate = async (data, id) => {
+Requests.InvoiceUpdate = async (data, id) => {
     try {
-        let res = await Request.put( endpoints.invoiceUpdate(id),  data);
+        let res = await Request.put(endpoints.invoiceUpdate(id), data);
         return res.data;
-        
+
     } catch (error) {
         checkAuth(error);
         throw new Error(error);
     }
-  }
+}
 
- Requests.InvoiceDelete = async (id) => {
+Requests.InvoiceDelete = async (id) => {
     try {
-        let res = await Request.delete( endpoints.invoiceDelete(id));
+        let res = await Request.delete(endpoints.invoiceDelete(id));
+        // console.log(res);
         return res.data;
-        
+
     } catch (error) {
         checkAuth(error);
         throw new Error(error);
     }
-  }
+}
 
- Requests.CompanyView = async (id) => {
+Requests.CompanyView = async (id) => {
     try {
-        let res = await Request.get( endpoints.companyView(id));
-        return res.data;
+        let res = await Request.get(endpoints.companyView(id));
+        // console.log(res.data.data[0]);
+        return res.data.data[0];
 
-        console.log(res.data);
-        
+
     } catch (error) {
         checkAuth(error);
         throw new Error(error);
     }
-  }
+}
 
-  function checkAuth(error) {
-    if (error.response.status == 401 || error.response.status == 403 ) {
-       return  window.location.href = "/login"
+function checkAuth(error) {
+    if (error.response.status == 401 || error.response.status == 403) {
+        return window.location.href = "/login"
     }
-  }
+}
 
 
-  export default Requests;
+export default Requests;
 
