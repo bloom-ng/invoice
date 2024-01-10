62d2bff25cd8719188207fb9018da08640cdab92
kingola1 <segun8427@gmail.com>
2024-01-10 17:20:03 +0100

    edited invoice view and fixed redirect issue

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,115 +2,129 @@ import React from "react";
 import Requests from "../../services/Requests";
 import { AuthContext } from "../../providers/auth/AuthProvider";
 import { useNavigate, Navigate } from "react-router-dom";
-import image from '../../assets/image.jpg'
-
-
+import image from "../../assets/image.jpg";
 
 const Login = () => {
-  const {dispatch} = React.useContext(AuthContext)
-  const navigate = useNavigate();
+	const { dispatch } = React.useContext(AuthContext);
+	const navigate = useNavigate();
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [loading, setLoading] = React.useState(false);
-  const [success, setSuccess] = React.useState(false);
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
+	const [loading, setLoading] = React.useState(false);
+	const [success, setSuccess] = React.useState(false);
 
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		try {
+			let response = await Requests.Login(email, password);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    setLoading(true)
-    try {
-      let response = await Requests.Login(email, password)      
+			localStorage.setItem("token", response.token);
+			localStorage.setItem("user", JSON.stringify(response.user));
+			// setSuccess(true)
+			// return navigate('/invoice');
+			// location.reload();
+			window.location.href = "/";
+		} catch (error) {
+			alert("Login Failed");
+			setLoading(false);
+		}
+	};
 
-     
-      localStorage.setItem('token', response.token)
-      localStorage.setItem('user', JSON.stringify(response.user))
-      // setSuccess(true)
-      // return navigate('/invoice');
-      location.reload();
-      
-    } catch (error) {
-      alert('Login Failed')
-      setLoading(false);
-
-    }
-  }
-
-
-  return (
-  <>
-  
-  <div className='w-full h-screen flex items-center'>
-            {success && (
-          <Navigate to="/" replace={true} />
-        )}
-          <div className='relative w-1/2 h-full flex flex-col sm:hidden lg:flex'>
-            {/* <div className="absolute top-[25%] left-[10%] flex flex-col">
+	return (
+		<>
+			<div className="w-full h-screen flex items-center">
+				{success && <Navigate to="/" replace={true} />}
+				<div className="relative w-1/2 h-full flex flex-col sm:hidden lg:flex">
+					{/* <div className="absolute top-[25%] left-[10%] flex flex-col">
               <h1 className='text-2xl'>Welcome To <span>Bloom Digital</span></h1>
             </div> */}
-            <img src={image} className='w-full h-full object-cover' alt="" />
-          </div>
-        
-        <div className='w-full h-full bg-zinc-100 flex flex-col p-20 items-center justify-between'>
-              <h1 className='text-xl text-black font-bold sm:text-2xl text-center'>
-              Welcome To <br/><span className='text-4xl lg:text-2xl text-orange-600 font-bold'>Bloom Digital Media</span>  
-              </h1>
-              
-                <div className='w-full flex flex-col max-w-[550px]'>
-                  <div className='w-full flex flex-col mb-2'>
+					<img
+						src={image}
+						className="w-full h-full object-cover"
+						alt=""
+					/>
+				</div>
 
-                <h3 className='text-5xl lg:text-2xl font-semibold mb-2'>Login</h3>
-                <p className='text-xl md:text-lg mb-2'>Welcome Back!</p>
-                </div>
-                <form onSubmit={handleLogin}>
-                  <div className="w-full flex flex-col">
-                    <input className='w-full text-black py-4 my-2 bg-transparent border-t-0 border-l-0 border-r-0
+				<div className="w-full h-full bg-zinc-100 flex flex-col p-20 items-center justify-between">
+					<h1 className="text-xl text-black font-bold sm:text-2xl text-center">
+						Welcome To <br />
+						<span className="text-4xl lg:text-2xl text-orange-600 font-bold">
+							Bloom Digital Media
+						</span>
+					</h1>
+
+					<div className="w-full flex flex-col max-w-[550px]">
+						<div className="w-full flex flex-col mb-2">
+							<h3 className="text-5xl lg:text-2xl font-semibold mb-2">
+								Login
+							</h3>
+							<p className="text-xl md:text-lg mb-2">
+								Welcome Back!
+							</p>
+						</div>
+						<form onSubmit={handleLogin}>
+							<div className="w-full flex flex-col">
+								<input
+									className="w-full text-black py-4 my-2 bg-transparent border-t-0 border-l-0 border-r-0
                      border-b-1 border-black
-                     outline-none focus:outline-none'  id="email-address"
-                     name="email"
-                     type="email"
-                     autoComplete="email"
-                     required   placeholder="Email address" onChange={e => setEmail(e.target.value)} />
+                     outline-none focus:outline-none"
+									id="email-address"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									placeholder="Email address"
+									onChange={(e) => setEmail(e.target.value)}
+								/>
 
-                    <input className='w-full text-black py-4 my-2 bg-transparent  border-t-0 border-l-0 border-r-0
+								<input
+									className="w-full text-black py-4 my-2 bg-transparent  border-t-0 border-l-0 border-r-0
                      border-b-1 border-black
-                     outline-none focus:outline-none '  id="password"
-                     name="password"
-                     type="password"
-                     autoComplete="current-password"
-                     required placeholder='Password'  onChange={e => setPassword(e.target.value)} />
+                     outline-none focus:outline-none "
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="current-password"
+									required
+									placeholder="Password"
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
+								/>
+							</div>
+							<div className="w-full flex items-center justify-between">
+								<div className="w-full flex items-center">
+									<input
+										type="checkbox"
+										className="w-4 h-4 mr-2"
+									/>
+									<p className="text-sm">Remember Me</p>
+								</div>
+								<p className="text-sm cursor-pointer underline underline-offset-2 whitespace-nowrap font-medium">
+									Forgot Password?
+								</p>
+							</div>
 
+							<div className="w-full flex flex-col my-4">
+								<button
+									type="submit"
+									className="text-white my-2 w-full bg-orange-600 
+                      rounded-md p-4 flex items-center justify-center font-bold hover:bg-black"
+								>
+									{loading ? "Signing In" : "Sign in"}
+								</button>
+							</div>
+						</form>
+					</div>
 
-                  </div>
-                  <div className='w-full flex items-center justify-between'>
-                    <div className='w-full flex items-center'>
-                      <input type="checkbox" className='w-4 h-4 mr-2' />
-                      <p className='text-sm'>Remember Me</p>
-                    </div>
-                    <p className='text-sm cursor-pointer underline underline-offset-2 whitespace-nowrap font-medium'>Forgot Password?</p>
-                  </div>
+					<div className="w-full flex items-center justify-center">
+						<p>Dont have an account?</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 
-                    <div className='w-full flex flex-col my-4'>
-                      <button type="submit" className='text-white my-2 w-full bg-orange-600 
-                      rounded-md p-4 flex items-center justify-center font-bold hover:bg-black'>
-                     
-                      {loading ? "Signing In" : "Sign in"} 
-                      </button>
-                    </div>
-                    </form>
-
-                </div>
-        
-            <div className="w-full flex items-center justify-center">
-              <p>Dont have an account?</p>
-            </div>
-
-        </div>
-
-    </div>
-    </>
-
-  )
-}
-
-export default Login
+export default Login;
--- src/pages/invoice/View.jsx
+++ src/pages/invoice/View.jsx
@@ -4,120 +4,123 @@ import Requests from "../../services/Requests";
 import BloomLogo from "../../assets/BloomLogo";
 
 function InvoiceView() {
-  const params = useParams();
-  const companyId = 1;
-  const [company, setCompany] = React.useState({});
-  const [invoice, setInvoice] = React.useState({});
-  const [lineItems, setLineItems] = React.useState([]);
+	const params = useParams();
+	const companyId = 1;
+	const [company, setCompany] = React.useState({});
+	const [invoice, setInvoice] = React.useState({});
+	const [lineItems, setLineItems] = React.useState([]);
 
-  React.useEffect(() => {
-    (async () => {
-      await getInvoice(params?.id);
-      await getCompany(companyId);
-    })();
-  }, []);
+	React.useEffect(() => {
+		(async () => {
+			await getInvoice(params?.id);
+			await getCompany(companyId);
+		})();
+	}, []);
 
-  const getInvoice = async (id) => {
-    try {
-      let res = await Requests.InvoiceView(id);
-      setInvoice(res);
+	const getInvoice = async (id) => {
+		try {
+			let res = await Requests.InvoiceView(id);
+			setInvoice(res);
 
-      setLineItems(JSON.parse(res.line_items));
-    } catch (error) {}
-  };
+			setLineItems(JSON.parse(res.line_items));
+		} catch (error) {}
+	};
 
-  const getCompany = async (id) => {
-    try {
-      let res = await Requests.CompanyView(id);
-      setCompany(res);
-    } catch (error) {}
-  };
+	const getCompany = async (id) => {
+		try {
+			let res = await Requests.CompanyView(id);
+			setCompany(res);
+		} catch (error) {}
+	};
 
-  const getSubtotal = () => {
-    let subtotal = 0;
-    lineItems.forEach((row) => {
-      subtotal += Number(row.price) * Number(row.qty);
-    });
+	const getSubtotal = () => {
+		let subtotal = 0;
+		lineItems.forEach((row) => {
+			subtotal += Number(row.price) * Number(row.qty);
+		});
 
-    return subtotal;
-  };
+		return subtotal;
+	};
 
-  const getVat = () => {
-    return (getSubtotal() * invoice.vat) / 100;
-  };
+	const getVat = () => {
+		return (getSubtotal() * invoice.vat) / 100;
+	};
 
-  const getServiceCharge = () => {
-    return (getSubtotal() * invoice.service_charge) / 100;
-  };
+	const getServiceCharge = () => {
+		return (getSubtotal() * invoice.service_charge) / 100;
+	};
 
-  const getNetTotal = () => {
-    return getSubtotal() + getVat() + getServiceCharge();
-  };
+	const getNetTotal = () => {
+		return getSubtotal() + getVat() + getServiceCharge();
+	};
 
-  const getDate = (date) => {
-    let d = new Date(date);
-    d = `${d.toDateString()}`;
-    d = d.slice(4);
+	const getDate = (date) => {
+		let d = new Date(date);
+		d = `${d.toDateString()}`;
+		d = d.slice(4);
 
-    return d;
-  };
+		return d;
+	};
 
-  function mainDate(invoiceDate) {
-    const originalDate = new Date(invoiceDate);
+	function mainDate(invoiceDate) {
+		const originalDate = new Date(invoiceDate);
 
-    const year = originalDate.getFullYear().toString();
-    const month = (originalDate.getMonth() + 1).toString().padStart(2, "0");
-    const day = originalDate.getDate().toString().padStart(2, "0");
+		const year = originalDate.getFullYear().toString();
+		const month = (originalDate.getMonth() + 1).toString().padStart(2, "0");
+		const day = originalDate.getDate().toString().padStart(2, "0");
 
-    const finalDate = year + month + day;
+		const finalDate = year + month + day;
 
-    return finalDate;
-  }
+		return finalDate;
+	}
 
-  return (
-    <div className="flex flex-col h-screen">
-      <div className="py-4 px-8 flex-grow">
-        <div className="flex justify-between">
-          {/* BLOOM LOGO */}
-          <div>
-            <div className="pr-20 mr-1">
-              <BloomLogo />
-            </div>
-            <div className="flex-col pr-8  mt-10">
-              <p className=" font-semibold">Bloom Digital Media,</p>
-              <p>{company?.address_line_1}</p>
-              <p>{company?.address_line_2}</p>
-              <p>{company?.address_line_3}</p>
-            </div>
-          </div>
+	return (
+		<div className="flex flex-col h-screen">
+			<div className="py-4 px-8 flex-grow">
+				<div className="flex justify-between">
+					{/* BLOOM LOGO */}
+					<div>
+						<div className="pr-20 mr-1">
+							<BloomLogo />
+						</div>
+						<div className="flex-col pr-8  mt-10">
+							<p className=" font-semibold">
+								Bloom Digital Media Ltd,
+							</p>
+							<p>{company?.address_line_1}</p>
+							<p>{company?.address_line_2}</p>
+							<p>{company?.address_line_3}</p>
+						</div>
+					</div>
 
-          <div className="">
-            <div className="mt-5">
-              <div>
-                <span className=" pb-0 font-extrabold text-5xl text-[#ff8100] uppercase">
-                  Invoice &nbsp;&nbsp;&nbsp;
-                </span>
-                <p className="">{getDate(invoice?.date)}</p>
-              </div>
-              <div className="flex-col mt-8">
-                <p className="font-normal my-1">
-                  Invoice No: INV{mainDate(invoice?.created_at)}
-                  {invoice?.id}
-                  {}
-                </p>
-                <div className="flex items-center gap-2 mt-2">
-                  <p className="font-semibold">To:</p>
-                  <p>{invoice?.billed_to_line_1}</p>
-                </div>
-                <p>{invoice?.billed_to_line_2}</p>
-                <p>{invoice?.billed_to_line_3}</p>
-              </div>
-            </div>
-          </div>
-        </div>
+					<div className="">
+						<div className="mt-5">
+							<div>
+								<span className=" pb-0 font-extrabold text-5xl text-[#ff8100] uppercase">
+									Invoice &nbsp;&nbsp;&nbsp;
+								</span>
+								<p className="">{getDate(invoice?.date)}</p>
+							</div>
+							<div className="flex-col mt-8">
+								<p className="font-normal my-1">
+									Invoice No: INV
+									{mainDate(invoice?.created_at)}
+									{invoice?.id}
+									{}
+								</p>
+								<div className="flex items-center gap-2 mt-2">
+									<p className="font-semibold">To:</p>
+									<p>{invoice?.billed_to_line_1}</p>
+								</div>
+								<p>{invoice?.billed_to_line_2}</p>
+								<p>{invoice?.billed_to_line_3}</p>
+							</div>
+						</div>
+					</div>
+				</div>
 
-        <div className="flex justify-between font-bold mt-2">
-          {/* <div className="flex-col">
+				<div className="flex justify-between font-bold mt-2">
+					{/* <div className="flex-col">
           <p className="font-normal my-1">
             Invoice No: INV{mainDate(invoice?.created_at)}
             {invoice?.id}
@@ -128,188 +131,166 @@ function InvoiceView() {
           <p>{invoice?.billed_to_line_2}</p>
           <p>{invoice?.billed_to_line_3}</p>
         </div> */}
-          <div className="flex-col mt-2 pr-8 ">
-            {/* <p>{company?.address_line_1}</p>
+					<div className="flex-col mt-2 pr-8 ">
+						{/* <p>{company?.address_line_1}</p>
           <p>{company?.address_line_2}</p>
           <p>{company?.address_line_3}</p> */}
 
-            {/* <p className="mt-6">{getDate(invoice?.date)}</p> */}
-          </div>
-        </div>
+						{/* <p className="mt-6">{getDate(invoice?.date)}</p> */}
+					</div>
+				</div>
 
-        <div className="border-b-[#ff8100] border-b-8 pt-8 pb-0"></div>
+				<div className="border-b-[#ff8100] border-b-8 pt-8 pb-0"></div>
 
-        <div></div>
+				<div></div>
 
-        <div className="relative">
-          <table className="w-full text-sm text-left text-black ">
-            <thead className="text-xs text-white uppercase bg-[#ff8100] ">
-              <tr>
-                <th scope="col" className="px-6 py-3">
-                  #
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Item Description
-                </th>
-                {/* <th scope="col" className="px-6 py-3">
+				<div className="relative">
+					<table className="w-full text-sm text-left text-black ">
+						<thead className="text-xs text-white uppercase bg-[#ff8100] ">
+							<tr>
+								<th scope="col" className="px-3 py-3">
+									#
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Item Description
+								</th>
+								{/* <th scope="col" className="px-6 py-3">
                   Description
                 </th> */}
-                <th scope="col" className="px-6 py-3">
-                  Unit Price &#8358;
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Qty
-                </th>
-                <th scope="col" className="px-6 py-3">
-                  Total &#8358;
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {lineItems?.length > 0 &&
-                lineItems?.map((row, index) => {
-                  return (
-                    <tr key={index} className="bg-white border-b border-black ">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                      >
-                        {index + 1}
-                      </th>
-                      <td className="px-6 py-4 w-80">
-                        {row?.item} <br /> {row?.desc}
-                      </td>
-                      {/* <td className="px-6 py-4">{row?.desc}</td> */}
-                      <td className="px-6 py-4">
-                        &#8358;{Number(row?.price).toLocaleString()}
-                      </td>
-                      <td className="px-6 py-4">{row?.qty}</td>
-                      <td className="px-6 py-4">
-                        &#8358;{(row?.qty * row?.price).toLocaleString()}
-                      </td>
-                    </tr>
-                  );
-                })}
+								<th scope="col" className="px-6 py-3">
+									Unit Price &#8358;
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Qty
+								</th>
+								<th scope="col" className="px-6 py-3">
+									Total &#8358;
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{lineItems?.length > 0 &&
+								lineItems?.map((row, index) => {
+									return (
+										<tr
+											key={index}
+											className="bg-white border-b border-black "
+										>
+											<th
+												scope="row"
+												className="w-10 px-3 py-4 font-medium text-gray-900 "
+											>
+												{index + 1}
+											</th>
+											<td className="px-6 py-4 w-full">
+												{row?.item} <br /> {row?.desc}
+											</td>
+											{/* <td className="px-6 py-4">{row?.desc}</td> */}
+											<td className="px-6 py-4">
+												&#8358;
+												{Number(
+													row?.price
+												).toLocaleString()}
+											</td>
+											<td className="px-6 py-4">
+												{row?.qty}
+											</td>
+											<td className="px-6 py-4">
+												&#8358;
+												{(
+													row?.qty * row?.price
+												).toLocaleString()}
+											</td>
+										</tr>
+									);
+								})}
+						</tbody>
+					</table>
+					<div className="flex items-center justify-between">
+						<div className="basis-2/4 self-start">
+							<p className="text-xs mt-5 font-bold self-start">
+								Note: Kindly pay to the Account number provided
+								in the invoice, kindly share payment receipt
+								after payment has been made. If for any reason a
+								refund is requested, administrative charge will
+								be deducted before refunds is made.
+							</p>
+						</div>
+						<div className="font-semibold basis-3/5 w-full items-center justify-center flex-col self-end">
+							<div className="flex items-center justify-between">
+								<p className="px-6 py-4">SUB TOTAL</p>
+								<p className="px-6 py-4">
+									&#8358;{getSubtotal().toLocaleString()}
+								</p>
+							</div>
+							<div className="flex items-center justify-between">
+								<p className="px-6 py-4">VAT</p>
+								<div className="px-6 py-4">{invoice?.vat}%</div>
+								<div className="px-6 py-4">
+									&#8358;
+									{parseFloat(
+										getVat().toFixed(2)
+									).toLocaleString()}
+								</div>
+							</div>
+							<div className="flex items-center justify-between">
+								<div className="px-6 py-4">SERVICE CHARGE</div>
+								<div className="px-6 py-4">
+									{invoice?.service_charge}%
+								</div>
+								<div className="px-6 py-4">
+									&#8358;
+									{parseFloat(
+										getServiceCharge().toFixed(2)
+									).toLocaleString()}
+								</div>
+							</div>
+							<div className="flex justify-between w-full bg-[#ff8100]">
+								<div className="px-6 py-4 text-white font-bold">
+									TOTAL DUE:
+								</div>
 
-              <tr>
-                <th scope="col" className="px-6 py-4">
-                <p className="text-left w-60 h-8 text-xs">
-                    Note: Kindly pay to the Account number provided in the
-                    invoice, kindly share payment receipt after payment has been
-                    made. If for any reason a refund is requested,
-                    administrative charge will be deducted before refunds is
-                    made.
-                  </p>
-                </th>
-                <th scope="col" className="px-6 py-4"></th>
-                {/* <th scope="col" className="px-6 py-4"></th> */}
-                <th scope="col" className="px-6 py-4">
-                  SUB TOTAL
-                </th>
-                <th scope="col" className="px-6 py-4"></th>
-                <th scope="col" className="px-6 py-4">
-                  &#8358;{getSubtotal().toLocaleString()}
-                </th>
-              </tr>
-              <tr>
-                <th scope="col" className="px-6 py-4"></th>
-                <th scope="col" className="px-6 py-4"></th>
-                {/* <th scope="col" className="px-6 py-4"></th> */}
-                <th scope="col" className="px-6 py-4">
-                  VAT
-                </th>
-                <th scope="col" className="px-6 py-4">
-                  {invoice?.vat}%
-                </th>
-                <th scope="col" className="px-6 py-4 ">
-                  &#8358;{parseFloat(getVat().toFixed(2)).toLocaleString()}
-                </th>
-              </tr>
-              <tr>
-                <th scope="col" className="px-6 py-4">
-                {/* <p className="text-left w-60 h-8 text-xs">
-                    Note: Kindly pay to the Account number provided in the
-                    invoice, kindly share payment receipt after payment has been
-                    made. If for any reason a refund is requested,
-                    administrative charge will be deducted before refunds is
-                    made.
-                  </p> */}
-                </th>
-                <th scope="col" className="px-6 py-4"></th>
-                {/* <th scope="col" className="px-6 py-4"></th> */}
-                <th scope="col" className="px-6 py-4">
-                  SERVICE CHARGE
-                </th>
-                <th scope="col" className="px-6 py-4">
-                  {invoice?.service_charge}%
-                </th>
-                <th scope="col" className="px-6 py-4">
-                  &#8358;
-                  {parseFloat(getServiceCharge().toFixed(2)).toLocaleString()}
-                </th>
-              </tr>
-              <tr className="">
-                <th scope="col" className="">
-                  {/* <p className="text-left w-60 h-8 text-xs">
-                    Note: Kindly pay to the Account number provided in the
-                    invoice, kindly share payment receipt after payment has been
-                    made. If for any reason a refund is requested,
-                    administrative charge will be deducted before refunds is
-                    made.
-                  </p> */}
-                </th>
-                <th scope="col" className="px-6 py-4"></th>
-                {/* <th scope="col" className="px-6 py-4"></th> */}
-                <th
-                  scope="col"
-                  className="px-6 py-4 border-t-2 bg-[#ff8100] text-white"
-                >
-                  TOTAL DUE:
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-4 border-t-2 bg-[#ff8100]"
-                ></th>
-                <th
-                  scope="col"
-                  className="px-6 py-4 border-t-2 bg-[#ff8100] text-white"
-                >
-                  &#8358;{" "}
-                  {parseFloat(getNetTotal().toFixed(2)).toLocaleString()}
-                </th>
-              </tr>
-            </tbody>
-          </table>
-          {/* <div className="border-b-orange-500 border-b-8 "></div> */}
-        </div>
+								<div className="px-6 py-4 text-white font-bold">
+									&#8358;{" "}
+									{parseFloat(
+										getNetTotal().toFixed(2)
+									).toLocaleString()}
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* <div className="border-b-orange-500 border-b-8 "></div> */}
+				</div>
 
-        {/* <div className="fixed bottom-5 left-20 flex">
+				{/* <div className="fixed bottom-5 left-20 flex">
         <p className="mx-4">Bloom Digital Media + (234) 7086278644</p>
         <p>Email- info@bloomdigitmedia.com</p>
     </div> */}
-        <div className="my-20">
-          <p className="font-bold">THANKS FOR YOUR PATRONAGE!</p>
-        </div>
-      </div>
-      <footer className="">
-        <div className="bg-[#ff8100] flex justify-between">
-          <div className="p-8 text-white">
-            <span className="font-bold uppercase">
-              PAY TO : &nbsp;&nbsp;&nbsp;
-            </span>
-            <p className="mt-4">{invoice?.account_name}</p>
-            <p>{invoice?.bank_name}</p>
-            <p>{invoice?.account_number}</p>
-          </div>
-          <div className="text-white mr-10 mt-6">
-            <span className="text-white font-bold">CONTACT DETAILS:</span>
-            <p className="">{company?.phone}</p>
-            <p>{company?.email}</p>
-          </div>
-        </div>
-      </footer>
-    </div>
-  );
+				<div className="my-20">
+					<p className="font-bold">THANKS FOR YOUR PATRONAGE!</p>
+				</div>
+			</div>
+			<footer className="">
+				<div className="bg-[#ff8100] flex justify-between">
+					<div className="p-8 text-white">
+						<span className="font-bold uppercase">
+							PAY TO : &nbsp;&nbsp;&nbsp;
+						</span>
+						<p className="mt-4">{invoice?.account_name}</p>
+						<p>{invoice?.bank_name}</p>
+						<p>{invoice?.account_number}</p>
+					</div>
+					<div className="text-white mr-10 mt-6">
+						<span className="text-white font-bold">
+							CONTACT DETAILS:
+						</span>
+						<p className="">{company?.phone}</p>
+						<p>{company?.email}</p>
+					</div>
+				</div>
+			</footer>
+		</div>
+	);
 }
 
 export default InvoiceView;
